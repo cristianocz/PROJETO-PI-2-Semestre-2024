fe2098e696af939e866487632e81a17a5c8f5849
Cristiano Cezar <cristianocz@hotmail.com>
2024-09-13 12:12:50 -0300

    configurando sistema para ajustes de controle

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ document.getElementById('consultaForm').addEventListener('submit', function(even
     const numeroPedido = document.getElementById('numeroPedido').value;
     
     // Simulação de consulta
-    if (numeroPedido === "12345") {
-        document.getElementById('statusOutput').innerHTML = `
-            <p>Status: Em conserto</p>
-            <p>Previsão de Retirada: 25/08/2024</p>
-        `;
-    } else {
+    if (numeroPedido === "12345") 
+//        {
+//        document.getElementById('statusOutput').innerHTML = `
+//            <p>Status: Em conserto</p>
+//            <p>Previsão de Retirada: 25/08/2024</p>
+//        `;}
+        {window.location.href = "./acompanha/acompanhamento.html";}
+     else {
         document.getElementById('statusOutput').innerHTML = `
             <p>Número do pedido não encontrado</p>
         `;
