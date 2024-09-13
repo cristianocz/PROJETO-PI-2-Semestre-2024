c3857569aa0b5eb5a3b82d421823d2e357828582
Cristiano Cezar <cristianocz@hotmail.com>
2024-09-13 15:25:35 -0300

    ajuste na chamada de pagina

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById('consultaForm').addEventListener('submit', function(event) {
+
+ document.getElementById('consultaForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita o envio do formulário
 
     const numeroPedido = document.getElementById('numeroPedido').value;
@@ -10,12 +11,11 @@ document.getElementById('consultaForm').addEventListener('submit', function(even
 //            <p>Status: Em conserto</p>
 //            <p>Previsão de Retirada: 25/08/2024</p>
 //        `;}
-//        {window.location.href = "./acompanha/acompanhamento.html";}
-        {window.location = "./acompanha/acompanhamento.html";}
-
+        {window.location.href = "acompanha/acompanhamento.html";}
      else {
         document.getElementById('statusOutput').innerHTML = `
             <p>Número do pedido não encontrado</p>
         `;
     }
 });
+
