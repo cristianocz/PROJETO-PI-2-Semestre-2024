36484ada4d80472b3d06d07dcff41a68b7154755
Cristiano Cezar <cristianocz@hotmail.com>
2024-09-13 15:28:30 -0300

    link para chamada

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 //            <p>Status: Em conserto</p>
 //            <p>Previsão de Retirada: 25/08/2024</p>
 //        `;}
-        {window.location.href = "acompanha/acompanhamento.html";}
+        {window.location.href = "./ACOMPANHA/acompanhamento.html";}
      else {
         document.getElementById('statusOutput').innerHTML = `
             <p>Número do pedido não encontrado</p>
